d7440a8c40d67c812e197900560bc6cd329beb4f
meruneru <meruneru1032@gmail.com>
2023-07-08 17:54:45 +0900

    add imports

--- src/handler.go
+++ src/handler.go
@@ -4,20 +4,22 @@ import (
 	"log"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/meruneru/notify-web-update/notifier"
+	"github.com/meruneru/notify-web-update/scraper"
 )
 
 func main() {
 	lambda.Start(Handler)
 }
 
-func Handler() {
-	isChanged, err := Scraping()
+func Handler() (string, error) {
+	isChanged, err := scraper.Scraping()
 	if err != nil {
 		log.Fatal("Error in Scraping: ", err)
 	}
 
 	if isChanged {
-		err = SendLineNotify("The item is available now!")
+		err = notifier.SendLineNotify("The item is available now!")
 		if err != nil {
 			log.Fatal("Error in sending Line Notify: ", err)
 		}
